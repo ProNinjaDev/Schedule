3e2265d59c4d3e47c93284b3ee7df66eb896c7b9
ProNinjaDev <pronin-04@bk.ru>
2024-10-18 22:36:45 +0300

    Переформировал структуру Program, добавил предметы

--- lab_work_inclass/Program.cs
+++ lab_work_inclass/Program.cs
@@ -7,43 +7,72 @@ using System.Threading.Tasks;
 //Это снейк кейс ^-^
 namespace lab_work_inclass
 {
-
+    
     internal class Program
     {
         internal const int NUMDAYS = 7;
         internal const int NUMPAIRS = 6;
         static void Main(string[] args)
         {
-            int numGroups, numSubjects, numLections, numPractics, numRemainingsPairs, numLectRooms, numTermRooms;
-            numGroups = 3;
-            numSubjects = 10;
-            numLections = 10;
-            numPractics = 5;
-            numRemainingsPairs = 4;
-            numLectRooms = 2;
-            numTermRooms = 1;
+            int numGroups = 3;
+            int numSubjects = 10;
+            int numLections = 10;
+            int numPractics = 5;
+            int numRemainingsPairs = 4;
+            int numLectRooms = 2;
+            int numTermRooms = 1;
 
+            List<Lectoriy> lectories = InitializeLectories(numLectRooms);
+            List<Terminal> terminals = InitializeTerminals(numTermRooms);
+            List<Subject> subjects = InitializeSubjects();
+            List<Group> groups = InitializeGroups(numGroups, subjects);
+        }
+
+        static List<Lectoriy> InitializeLectories(int numLectRooms)
+        {
             List<Lectoriy> lectories = new List<Lectoriy>();
             for (int i = 0; i < numLectRooms; i++)
             {
                 lectories.Add(new Lectoriy(i + 1));
             }
+            return lectories;
+        }
 
+        static List<Terminal> InitializeTerminals(int numTermRooms)
+        {
             List<Terminal> terminals = new List<Terminal>();
             for (int i = 0; i < numTermRooms; i++)
-            {
+            { 
                 terminals.Add(new Terminal(i + 1));
             }
+            return terminals;
+        }
 
-            // todo: Заполнить subjects и перенести в отдельный метод (или даже класс)
-            List<Subject> subjects = new List<Subject>();
-
-            List<Group> groups = new List<Group>();
-            for (int i = 0;i < numGroups; i++)
+        static List<Subject> InitializeSubjects()
+        {
+            return new List<Subject>()
             {
-                //groups.Add(new Group(i + 1, ))
-            }
+                new Subject("Теория вычислимости", 1, 0),
+                new Subject("Теория вероятностей", 1, 1),
+                new Subject("Мат. основы автоматизации УПИМ", 1, 1),
+                new Subject("КСЕ", 1, 0),
+                new Subject("Методы оптимизации", 1, 1),
+                new Subject("Физика", 1, 1),
+                new Subject("ЭГА", 1, 0),
+                new Subject("Проектирование ИС", 1, 0),
+                new Subject("Высокоуровневые методы программирования", 1, 1),
+                new Subject("Математический анализ", 1, 0)
+            };
+        }
 
+        static List<Group> InitializeGroups(int numGroups, List<Subject> subjects)
+        {
+            List<Group> groups = new List<Group>();
+            for (int i = 0; i < numGroups; i++)
+            {
+                groups.Add(new Group(i + 1, subjects));
+            }
+            return groups;
         }
     }
 }
